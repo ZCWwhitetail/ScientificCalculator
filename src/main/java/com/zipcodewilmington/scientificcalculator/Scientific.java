105a635b818763d5b73c75d232ddcf36001484e2
Corinne <corinne@Zipcoders-MacBook-Pro.local>
2023-10-28 16:08:49 -0400

    working on scientific functions

--- src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -1,4 +1,66 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Scientific {
+
+    public static void switchDisplayMode(){
+        //not sure what this means yet
+    }
+
+    public static void switchDisplayMode(String mode){
+        //not sure what this means yet
+    }
+
+    public static void memoryAdd(Double input){
+        //memory = input;
+        //Where is memory stored though? Should this be in console/main?
+    }
+
+    public static void memoryClear(){
+        //memory = 0;
+    }
+
+    public static double memoryRecall(){
+        return memory;
+    }
+
+    public static double sine(double input){
+        return Math.sin(input);
+    }
+
+    public static double cosine(double input){
+        return Math.cos(input);
+    }
+
+    public static double tan(double input) {
+        return Math.tan(input);
+    }
+
+    public static double asin(double input) {
+        return Math.asin(input);
+    }
+
+    public static double acos(double input) {
+        return Math.acos(input);
+    }
+
+    public static double atan(double input) {
+        return Math.atan(input);
+    }
+
+    public static void switchUnitsMode() {
+        //should rotate through the options
+    }
+    public static void switchUnitsMode(String mode) {
+        // should set the trig units to the type given
+    }
+
+    public static double log(double input) {
+        return Math.log10(input);
+    }
+
+    public static double inverseLog(double input) {
+        return Math.pow(10, input);
+    }
+
+    
 }
