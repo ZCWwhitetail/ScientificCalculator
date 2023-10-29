41a1b9348021ded4e7510aa62e9a2935ed9deca7
Joey <joey@Zipcoders-MacBook-Pro-9.local>
2023-10-29 13:01:05 -0400

    made changes and completed code

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,54 @@
 package com.zipcodewilmington.scientificcalculator;
+import java.util.Scanner;
+import static java.lang.Integer.*;
 
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+        Scanner scanner =new Scanner(System.in);
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        while(true) {
+            System.out.println("Enter first value");
+            final String value1 = scanner.nextLine();
+            final Double doubleValue1 = Double.parseDouble(value1);
+
+            System.out.println("Enter second value");
+            final String value2 = scanner.nextLine();
+            final Double doubleValue2 = Double.parseDouble(value2);
+
+            System.out.println(String.format("Enter operation: [*, /, +, -]"));
+            final String selectedOperation = scanner.nextLine();
+
+            Double result = 0.0;
+
+            if ("+".equals(selectedOperation)) {
+                result = doubleValue1 + doubleValue2;
+            } else if ("-".equals(selectedOperation)) {
+                result = doubleValue1 - doubleValue2;
+            } else if (("*").equals(selectedOperation)) {
+                result = doubleValue1 * doubleValue2;
+            } else if ("/".equals(selectedOperation)) {
+                if (doubleValue2 == 0) {
+                    System.out.println("Error: Division by zero.");
+                    continue;
+                }
+                result = doubleValue1 / doubleValue2;
+            } else {
+                System.out.println ("Invalid operation. Please choose valid operation");
+                continue;
+            }
+            System.out.println("Your answer = " + result);
+        }
+
+        }
+        public static Double parseDouble(String value) {
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+        }
+
     }
-}
